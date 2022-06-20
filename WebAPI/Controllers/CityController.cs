db6201f1aeab5131a980433bbf240a11d70f00a3
Roman Kiselyk <rkiselyk@vector-software.com>
2022-06-20 18:41:31 +0300

    Create base template for WebApi

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New York", "Atlanta" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "New York";
+        }
+    }
+}
